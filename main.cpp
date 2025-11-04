2deaf0076392f7444ea2536053fd42efa3063cb6
erictare <erict1006@gmail.com>
2025-11-03 19:19:54 -0500

    changed main.cpp and quicksort.cpp to be able to sort by multiple filters. commented out old chunks of code in case we want to refer back. should be safe to delete

--- main.cpp
+++ main.cpp
@@ -47,152 +47,242 @@ int main() {
         customers.emplace_back(income, age, gender, country, avg_spent, category, credit_score, total, id);
     }
     file.close();
+    //info from csv file is grabbed and put into a vector of Customers
 
+
+
+    //get filter, we want to make it so that you incrementally add filters, one after another, not all in one go.
     while (true) {
-        cout << "Please enter your chosen filters:\n(Default, Age, Gender, Country, Average Spent, Category, Credit Score, or Total Spent)" << endl;
+        cout << "\nPlease enter your chosen filters:\n(Income, Age, Gender, Country, Average Spent, Category, Credit Score, or Total Spent)" << endl;
         getline(cin >> ws, input);
-        if (input == "Default") {
-            auto start_time = chrono::high_resolution_clock::now();
 
-            quicksort(customers, 0, customers.size()-1, 0);
-            auto end_time = chrono::high_resolution_clock::now();
-            auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
-            ofstream outfile("quicksorted_customers.csv");
-            if (!outfile.is_open()) {
-                cerr << "Error opening file!" << endl;
-            }
-            outfile << "Income" << endl;
-            for (int i = 0; i < customers.size(); i++) {
-                outfile << customers[i].getID() << ": $" << customers[i].getIncome() << endl;
-            }
 
-            cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
+        //vector of all filters
+        vector<int> filters;
+        stringstream ss(input);
+        string filter_name;
+        while (getline(ss, filter_name, ',')) {
+
+            filter_name.erase(0, filter_name.find_first_not_of(" \t\n\r"));     //rid any spaces
+            filter_name.erase(filter_name.find_last_not_of(" \t\n\r") + 1);
+
+            if (filter_name == "Income") filters.push_back(0);
+            else if (filter_name == "Age") filters.push_back(1);
+            else if (filter_name == "Gender") filters.push_back(2);
+            else if (filter_name == "Country") filters.push_back(3);
+            else if (filter_name == "Average Spent") filters.push_back(4);
+            else if (filter_name == "Category") filters.push_back(5);
+            else if (filter_name == "Credit Score") filters.push_back(6);
+            else if (filter_name == "Total Spent") filters.push_back(7);
+            else cout << "Filter " << filter_name << " does not exist" << endl;
         }
-        else if (input == "Age") {
-            auto start_time = chrono::high_resolution_clock::now();
 
-            quicksort(customers, 0, customers.size()-1, 1);
-            auto end_time = chrono::high_resolution_clock::now();
-            auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
-            ofstream outfile("quicksorted_customers.csv");
-            if (!outfile.is_open()) {
-                cerr << "Error opening file!" << endl;
-            }
-            outfile << "Age" << endl;
-            for (int i = 0; i < customers.size(); i++) {
-                outfile << customers[i].getID() << " Age: " << customers[i].getAge() << ", Income: $" << customers[i].getIncome() << endl;
-            }
+        if (filters.empty()) continue; //ask again
 
-            cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
+        //timer
+        auto start_time = chrono::high_resolution_clock::now();
+        quicksort(customers, 0, customers.size()-1, filters); //fix this
+        auto end_time = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+        cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
+
+        //write sort to file
+        ofstream outfile("quicksorted_customers.csv");
+
+        //header
+        outfile << "ID"; //always has their id in it
+        for (int f : filters) {
+            if (f==0) outfile << ", Income";
+            else if (f==1) outfile << ", Age";
+            else if (f==2) outfile << ", Gender";
+            else if (f==3) outfile << ", Country";
+            else if (f==4) outfile << ", Average Spent";
+            else if (f==5) outfile << ", Category";
+            else if (f==6) outfile << ", Credit Score";
+            else if (f==7) outfile << ", Total Spent";
         }
-        else if (input == "Gender") {
-            auto start_time = chrono::high_resolution_clock::now();
+        outfile << "\n";
 
-            quicksort(customers, 0, customers.size()-1, 2);
-            auto end_time = chrono::high_resolution_clock::now();
-            auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
-            ofstream outfile("quicksorted_customers.csv");
-            if (!outfile.is_open()) {
-                cerr << "Error opening file!" << endl;
-            }
-            outfile << "Gender" << endl;
-            for (int i = 0; i < customers.size(); i++) {
-                outfile << customers[i].getID() << " Gender: " << customers[i].getGender() << ", Income: $" << customers[i].getIncome() << endl;
-            }
+        //actual body, it sorts things in the order u give it (IMPORTANT!!!!)_________________________________________________________________________________________________
+        //example if its (country, age, income) they sort in that order
+        //if two customers have same country, they sort by age, and so forth
+        //changing the order of the filters changes the priority of sorting
 
-            cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
+        for (Customer c : customers) {
+            outfile << c.getID();
+            for (int f : filters) {
+                if (f==0) outfile << " - $" << c.getIncome();
+                else if (f==1) outfile << " - " << c.getAge();
+                else if (f==2) outfile << " - " << c.getGender();
+                else if (f==3) outfile << " - " << c.getCountry();
+                else if (f==4) outfile << " - $" << c.getAvgSpent();
+                else if (f==5) outfile << " - " << c.getCategory();
+                else if (f==6) outfile << " - " << c.getCreditScore();
+                else if (f==7) outfile << " - $" << c.getTotalSpent();
+            }
+            outfile << "\n";
         }
-        else if (input == "Country") {
-            auto start_time = chrono::high_resolution_clock::now();
+        outfile.close();
 
-            quicksort(customers, 0, customers.size()-1, 3);
-            auto end_time = chrono::high_resolution_clock::now();
-            auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
-            ofstream outfile("quicksorted_customers.csv");
-            if (!outfile.is_open()) {
-                cerr << "Error opening file!" << endl;
-            }
-            outfile << "Country" << endl;
-            for (int i = 0; i < customers.size(); i++) {
-                outfile << customers[i].getID() << " Country: " << customers[i].getCountry() << ", Income: $" << customers[i].getIncome() << endl;
-            }
+        //commenting this to try and implement several filters, safe to delete
 
-            cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
-        }
-        else if (input == "Average Spent") {
-            auto start_time = chrono::high_resolution_clock::now();
-
-            quicksort(customers, 0, customers.size()-1, 4);
-            auto end_time = chrono::high_resolution_clock::now();
-            auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
-            ofstream outfile("quicksorted_customers.csv");
-            if (!outfile.is_open()) {
-                cerr << "Error opening file!" << endl;
-            }
-            outfile << "Average Spent Per Order" << endl;
-            for (int i = 0; i < customers.size(); i++) {
-                outfile << customers[i].getID() << " Average Spent: $" << customers[i].getAvgSpent() << ", Income: $" << customers[i].getIncome() << endl;
-            }
-
-            cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
-        }
-        else if (input == "Category") {
-            auto start_time = chrono::high_resolution_clock::now();
-
-            quicksort(customers, 0, customers.size()-1, 5);
-            auto end_time = chrono::high_resolution_clock::now();
-            auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
-            ofstream outfile("quicksorted_customers.csv");
-            if (!outfile.is_open()) {
-                cerr << "Error opening file!" << endl;
-            }
-            outfile << "Most Common Category of Purchase" << endl;
-            for (int i = 0; i < customers.size(); i++) {
-                outfile << customers[i].getID() << " Category: " << customers[i].getAge() << ", Income: $" << customers[i].getIncome() << endl;
-            }
-
-            cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
-        }
-        else if (input == "Credit Score") {
-            auto start_time = chrono::high_resolution_clock::now();
-
-            quicksort(customers, 0, customers.size()-1, 6);
-            auto end_time = chrono::high_resolution_clock::now();
-            auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
-            ofstream outfile("quicksorted_customers.csv");
-            if (!outfile.is_open()) {
-                cerr << "Error opening file!" << endl;
-            }
-            outfile << "Credit Score" << endl;
-            for (int i = 0; i < customers.size(); i++) {
-                outfile << customers[i].getID() << " Credit Score: " << customers[i].getCreditScore() << ", Income: $" << customers[i].getIncome() << endl;
-            }
-
-            cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
-        }
-        else if (input == "Total Spent") {
-            auto start_time = chrono::high_resolution_clock::now();
-
-            quicksort(customers, 0, customers.size()-1, 7);
-            auto end_time = chrono::high_resolution_clock::now();
-            auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
-            ofstream outfile("quicksorted_customers.csv");
-            if (!outfile.is_open()) {
-                cerr << "Error opening file!" << endl;
-            }
-            outfile << "Total Spent" << endl;
-            for (int i = 0; i < customers.size(); i++) {
-                outfile << customers[i].getID() << " Total Spent: $" << customers[i].getTotalSpent() << ", Income: $" << customers[i].getIncome() << endl;
-            }
-
-            cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
-        }
-        else if (input == "Exit") {
-            break;
-        }
-        else {
-            cout << "Invalid input!" << endl;
-        }
+        // //differentiates inputs
+        // if (input == "Default") {
+        //     auto start_time = chrono::high_resolution_clock::now();
+        //
+        //     quicksort(customers, 0, customers.size()-1, 0);
+        //     auto end_time = chrono::high_resolution_clock::now();
+        //     auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+        //     ofstream outfile("quicksorted_customers.csv");
+        //     if (!outfile.is_open()) {
+        //         cerr << "Error opening file!" << endl;
+        //     }
+        //     outfile << "Income" << endl;
+        //     for (int i = 0; i < customers.size(); i++) {
+        //         outfile << customers[i].getID() << ": $" << customers[i].getIncome() << endl;
+        //     }
+        //
+        //     cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
+        // }
+        //
+        //
+        // else if (input == "Age") {
+        //     auto start_time = chrono::high_resolution_clock::now();
+        //
+        //     quicksort(customers, 0, customers.size()-1, 1);
+        //     auto end_time = chrono::high_resolution_clock::now();
+        //     auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+        //     ofstream outfile("quicksorted_customers.csv");
+        //     if (!outfile.is_open()) {
+        //         cerr << "Error opening file!" << endl;
+        //     }
+        //     outfile << "Age" << endl;
+        //     for (int i = 0; i < customers.size(); i++) {
+        //         outfile << customers[i].getID() << " Age: " << customers[i].getAge() << ", Income: $" << customers[i].getIncome() << endl;
+        //     }
+        //
+        //     cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
+        // }
+        //
+        //
+        // else if (input == "Gender") {
+        //     auto start_time = chrono::high_resolution_clock::now();
+        //
+        //     quicksort(customers, 0, customers.size()-1, 2);
+        //     auto end_time = chrono::high_resolution_clock::now();
+        //     auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+        //     ofstream outfile("quicksorted_customers.csv");
+        //     if (!outfile.is_open()) {
+        //         cerr << "Error opening file!" << endl;
+        //     }
+        //     outfile << "Gender" << endl;
+        //     for (int i = 0; i < customers.size(); i++) {
+        //         outfile << customers[i].getID() << " Gender: " << customers[i].getGender() << ", Income: $" << customers[i].getIncome() << endl;
+        //     }
+        //
+        //     cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
+        // }
+        //
+        //
+        // else if (input == "Country") {
+        //     auto start_time = chrono::high_resolution_clock::now();
+        //
+        //     quicksort(customers, 0, customers.size()-1, 3);
+        //     auto end_time = chrono::high_resolution_clock::now();
+        //     auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+        //     ofstream outfile("quicksorted_customers.csv");
+        //     if (!outfile.is_open()) {
+        //         cerr << "Error opening file!" << endl;
+        //     }
+        //     outfile << "Country" << endl;
+        //     for (int i = 0; i < customers.size(); i++) {
+        //         outfile << customers[i].getID() << " Country: " << customers[i].getCountry() << ", Income: $" << customers[i].getIncome() << endl;
+        //     }
+        //
+        //     cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
+        // }
+        //
+        //
+        // else if (input == "Average Spent") {
+        //     auto start_time = chrono::high_resolution_clock::now();
+        //
+        //     quicksort(customers, 0, customers.size()-1, 4);
+        //     auto end_time = chrono::high_resolution_clock::now();
+        //     auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+        //     ofstream outfile("quicksorted_customers.csv");
+        //     if (!outfile.is_open()) {
+        //         cerr << "Error opening file!" << endl;
+        //     }
+        //     outfile << "Average Spent Per Order" << endl;
+        //     for (int i = 0; i < customers.size(); i++) {
+        //         outfile << customers[i].getID() << " Average Spent: $" << customers[i].getAvgSpent() << ", Income: $" << customers[i].getIncome() << endl;
+        //     }
+        //
+        //     cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
+        // }
+        //
+        //
+        // else if (input == "Category") {
+        //     auto start_time = chrono::high_resolution_clock::now();
+        //
+        //     quicksort(customers, 0, customers.size()-1, 5);
+        //     auto end_time = chrono::high_resolution_clock::now();
+        //     auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+        //     ofstream outfile("quicksorted_customers.csv");
+        //     if (!outfile.is_open()) {
+        //         cerr << "Error opening file!" << endl;
+        //     }
+        //     outfile << "Most Common Category of Purchase" << endl;
+        //     for (int i = 0; i < customers.size(); i++) {
+        //         outfile << customers[i].getID() << " Category: " << customers[i].getAge() << ", Income: $" << customers[i].getIncome() << endl;
+        //     }
+        //
+        //     cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
+        // }
+        //
+        //
+        // else if (input == "Credit Score") {
+        //     auto start_time = chrono::high_resolution_clock::now();
+        //
+        //     quicksort(customers, 0, customers.size()-1, 6);
+        //     auto end_time = chrono::high_resolution_clock::now();
+        //     auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+        //     ofstream outfile("quicksorted_customers.csv");
+        //     if (!outfile.is_open()) {
+        //         cerr << "Error opening file!" << endl;
+        //     }
+        //     outfile << "Credit Score" << endl;
+        //     for (int i = 0; i < customers.size(); i++) {
+        //         outfile << customers[i].getID() << " Credit Score: " << customers[i].getCreditScore() << ", Income: $" << customers[i].getIncome() << endl;
+        //     }
+        //
+        //     cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
+        // }
+        //
+        //
+        // else if (input == "Total Spent") {
+        //     auto start_time = chrono::high_resolution_clock::now();
+        //
+        //     quicksort(customers, 0, customers.size()-1, 7);
+        //     auto end_time = chrono::high_resolution_clock::now();
+        //     auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+        //     ofstream outfile("quicksorted_customers.csv");
+        //     if (!outfile.is_open()) {
+        //         cerr << "Error opening file!" << endl;
+        //     }
+        //     outfile << "Total Spent" << endl;
+        //     for (int i = 0; i < customers.size(); i++) {
+        //         outfile << customers[i].getID() << " Total Spent: $" << customers[i].getTotalSpent() << ", Income: $" << customers[i].getIncome() << endl;
+        //     }
+        //
+        //     cout << "Quicksort Execution time: " << duration.count() * 0.000001 << " seconds" << endl;
+        // }
+        // else if (input == "Exit") {
+        //     break;
+        // }
+        // else {
+        //     cout << "Invalid input!" << endl;
+        // }
     }
 
     return 0;
--- quicksort.cpp
+++ quicksort.cpp
@@ -7,85 +7,147 @@
 
 using namespace std;
 
-bool isLess(Customer& c1, Customer& c2, int cat) {
-    if (cat == 0) {
-        return c1.getIncome() < c2.getIncome();
+bool isLess(Customer& c1, Customer& c2, const vector<int>& filters) {
+    for (int f : filters) {
+
+        if (f == 0) {
+            if (c1.getIncome() < c2.getIncome())
+                return true;
+            if (c1.getIncome() > c2.getIncome())
+                return false;
+        }
+
+        if (f == 1) {
+            if (c1.getAge() < c2.getAge())
+                return true;
+            if (c1.getAge() > c2.getAge())
+                return false;
+        }
+
+        if (f == 2) {
+            if (c1.getGender() < c2.getGender())
+                return true;
+            if (c1.getGender() > c2.getGender())
+                return false;
+        }
+
+        if (f == 3) {
+            if (c1.getCountry() < c2.getCountry())
+                return true;
+            if (c1.getCountry() > c2.getCountry())
+                return false;
+        }
+
+        if (f == 4) {
+            if (c1.getAvgSpent() < c2.getAvgSpent())
+                return true;
+            if (c1.getAvgSpent() > c2.getAvgSpent())
+                return false;
+        }
+
+        if (f == 5) {
+            if (c1.getCategory() < c2.getCategory())
+                return true;
+            if (c1.getCategory() > c2.getCategory())
+                return false;
+        }
+
+        if (f == 6) {
+            if (c1.getCreditScore() < c2.getCreditScore())
+                return true;
+            if (c1.getCreditScore() > c2.getCreditScore())
+                return false;
+        }
+
+        if (f == 7) {
+            if (c1.getTotalSpent() < c2.getTotalSpent())
+                return true;
+            if (c1.getTotalSpent() > c2.getTotalSpent())
+                return false;
+        }
     }
-    else if (cat == 1) {
-        if (c1.getAge() < c2.getAge()) {
-            return true;
-        }
-        if (c1.getAge() > c2.getAge()) {
-            return false;
-        }
-        return c1.getIncome() < c2.getIncome();
-    }
-    else if (cat == 2) {
-        if (c1.getGender() < c2.getGender()) {
-            return true;
-        }
-        if (c1.getGender() > c2.getGender()) {
-            return false;
-        }
-        return c1.getIncome() < c2.getIncome();
-    }
-    else if (cat == 3) {
-        if (c1.getCountry() < c2.getCountry()) {
-            return true;
-        }
-        if (c1.getCountry() > c2.getCountry()) {
-            return false;
-        }
-        return c1.getIncome() < c2.getIncome();
-    }
-    else if (cat == 4) {
-        if (c1.getAvgSpent() < c2.getAvgSpent()) {
-            return true;
-        }
-        if (c1.getAvgSpent() > c2.getAvgSpent()) {
-            return false;
-        }
-        return c1.getIncome() < c2.getIncome();
-    }
-    else if (cat == 5) {
-        if (c1.getCategory() < c2.getCategory()) {
-            return true;
-        }
-        if (c1.getCategory() > c2.getCategory()) {
-            return false;
-        }
-        return c1.getIncome() < c2.getIncome();
-    }
-    else if (cat == 6) {
-        if (c1.getCreditScore() < c2.getCreditScore()) {
-            return true;
-        }
-        if (c1.getCreditScore() > c2.getCreditScore()) {
-            return false;
-        }
-        return c1.getIncome() < c2.getIncome();
-    }
-    else if (cat == 7) {
-        if (c1.getTotalSpent() < c2.getTotalSpent()) {
-            return true;
-        }
-        if (c1.getTotalSpent() > c2.getTotalSpent()) {
-            return false;
-        }
-        return c1.getIncome() < c2.getIncome();
-    }
+    return false;
+
+    //commenting this to try and implement several filters, safe to delete
+
+    // if (cat == 0) {
+    //     return c1.getIncome() < c2.getIncome();
+    // }
+    // else if (cat == 1) {
+    //     if (c1.getAge() < c2.getAge()) {
+    //         return true;
+    //     }
+    //     if (c1.getAge() > c2.getAge()) {
+    //         return false;
+    //     }
+    //     return c1.getIncome() < c2.getIncome();
+    // }
+    // else if (cat == 2) {
+    //     if (c1.getGender() < c2.getGender()) {
+    //         return true;
+    //     }
+    //     if (c1.getGender() > c2.getGender()) {
+    //         return false;
+    //     }
+    //     return c1.getIncome() < c2.getIncome();
+    // }
+    // else if (cat == 3) {
+    //     if (c1.getCountry() < c2.getCountry()) {
+    //         return true;
+    //     }
+    //     if (c1.getCountry() > c2.getCountry()) {
+    //         return false;
+    //     }
+    //     return c1.getIncome() < c2.getIncome();
+    // }
+    // else if (cat == 4) {
+    //     if (c1.getAvgSpent() < c2.getAvgSpent()) {
+    //         return true;
+    //     }
+    //     if (c1.getAvgSpent() > c2.getAvgSpent()) {
+    //         return false;
+    //     }
+    //     return c1.getIncome() < c2.getIncome();
+    // }
+    // else if (cat == 5) {
+    //     if (c1.getCategory() < c2.getCategory()) {
+    //         return true;
+    //     }
+    //     if (c1.getCategory() > c2.getCategory()) {
+    //         return false;
+    //     }
+    //     return c1.getIncome() < c2.getIncome();
+    // }
+    // else if (cat == 6) {
+    //     if (c1.getCreditScore() < c2.getCreditScore()) {
+    //         return true;
+    //     }
+    //     if (c1.getCreditScore() > c2.getCreditScore()) {
+    //         return false;
+    //     }
+    //     return c1.getIncome() < c2.getIncome();
+    // }
+    // else if (cat == 7) {
+    //     if (c1.getTotalSpent() < c2.getTotalSpent()) {
+    //         return true;
+    //     }
+    //     if (c1.getTotalSpent() > c2.getTotalSpent()) {
+    //         return false;
+    //     }
+    //     return c1.getIncome() < c2.getIncome();
+    // }
 
 }
 
-void quicksort(vector<Customer>& v, int left, int right, int cat) {
+void quicksort(vector<Customer>& v, int left, int right, const vector<int>& filters) {
     if (left < right) {
-        int pivot = partition(v, left, right, cat);
-        quicksort(v, left, pivot-1, cat);
-        quicksort(v, pivot+1, right, cat);
+        int pivot = partition(v, left, right, filters);
+        quicksort(v, left, pivot-1, filters);
+        quicksort(v, pivot+1, right, filters);
     }
 }
 
-int partition(vector<Customer>& v, int low, int high, int cat) {
+int partition(vector<Customer>& v, int low, int high, const vector<int>& filters) {
     int mid = (low + high) / 2;
 
     Customer& first = v[low];
@@ -93,10 +155,10 @@ int partition(vector<Customer>& v, int low, int high, int cat) {
     Customer& last = v[high];
 
     int p;
-    if ((isLess(first,second, cat) && isLess(second, last, cat)) || (isLess(last, second, cat) && isLess(second, first, cat))) {
+    if ((isLess(first,second, filters) && isLess(second, last, filters)) || (isLess(last, second, filters) && isLess(second, first, filters))) {
         p = mid;
     }
-    else if ((isLess(second, first, cat) && isLess(first, last, cat)) || (isLess(last, first, cat) && isLess(first, second, cat))) {
+    else if ((isLess(second, first, filters) && isLess(first, last, filters)) || (isLess(last, first, filters) && isLess(first, second, filters))) {
         p = low;
     }
     else {
@@ -108,7 +170,7 @@ int partition(vector<Customer>& v, int low, int high, int cat) {
     int i = low - 1;
 
     for (int j = low; j < high; j++) {
-        if (isLess(v[j], pivot, cat)) {
+        if (isLess(v[j], pivot, filters)) {
             i++;
             swap(v[i], v[j]);
         }
